97711fa1829f7d9164c39c5a09385db845f9a6a5
naamShahreyar <shahreyar1411@gmail.com>
2026-07-11 16:30:16 +0530

    Update: Config

--- config/gpt_config.py
+++ config/gpt_config.py
@@ -4,8 +4,8 @@ from dataclasses import dataclass
 @dataclass
 class GPTConfig:
     vocab_size: int = 8192
-    context_length: int = 256
-    d_model: int = 512
+    context_length: int = 512
+    d_model: int = 768
     n_heads: int = 8
     n_layers: int = 8
     dropout: float = 0.1
